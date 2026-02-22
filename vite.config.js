dcac73372fcd7d954d9f3c672df92e6fa287b11d
Somon Karimzoda <karimzoda@example.com>
2026-02-23 04:14:38 +0500

    Coffee-Shop

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-// vite.config.js
 import { defineConfig } from "vite";
 
-export default defineConfig({
-  base: "/Coffee-Shop/",
-  server: { port: 5173, open: true },
+export default defineConfig(() => {
+  // В GitHub Actions есть переменная: "owner/repo"
+  const repo = process.env.GITHUB_REPOSITORY?.split("/")[1];
+
+  // Если это User Pages репо (username.github.io) → base должен быть "/"
+  const isUserPages = repo?.endsWith(".github.io");
+
+  return {
+    base: repo && !isUserPages ? `/${repo}/` : "/",
+    server: { port: 5173, open: true },
+  };
 });
