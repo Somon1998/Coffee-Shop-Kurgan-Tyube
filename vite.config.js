cac0cfd9c615d9a188455e73da7516ff5ff20e0e
Somon Karimzoda <karimzoda@example.com>
2026-02-23 04:29:17 +0500

    auto base for github pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig(() => {
-  // В GitHub Actions есть переменная: "owner/repo"
   const repo = process.env.GITHUB_REPOSITORY?.split("/")[1];
-
-  // Если это User Pages репо (username.github.io) → base должен быть "/"
   const isUserPages = repo?.endsWith(".github.io");
 
   return {
